8d06d5b796009915e666dab073334a6ddf709a77
Kirill Dubovikov <kdubovikov@cinimex.ru>
2020-04-26 11:58:09 +0300

    added bme280 driver

--- main/weather_station.c
+++ main/weather_station.c
@@ -21,6 +21,10 @@
 
 #include "mqtt_client.h"
 
+#include "bme280.h"
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+
 /* Set the SSID and Password via project configuration, or can set directly here */
 #define DEFAULT_SSID "MGTS_GPON_39F0"
 #define DEFAULT_PWD "3gYgqSeA"
@@ -59,7 +63,14 @@
 #define DEFAULT_AUTHMODE WIFI_AUTH_OPEN
 #endif /*CONFIG_EXAMPLE_FAST_SCAN_THRESHOLD*/
 
-#define CONNECTED_BITS BIT(1)
+#define WIFI_CONNECTED_BITS BIT(1)
+
+// BME280 constants
+#define TAG_BME280 "BME280"
+#define I2C_MASTER_ACK 0
+#define I2C_MASTER_NACK 1
+#define SDA_PIN GPIO_NUM_27
+#define SCL_PIN GPIO_NUM_26
 
 static const char *TAG = "weather_station";
 static EventGroupHandle_t s_connect_event_group;
@@ -130,7 +141,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
         ESP_LOGI(TAG, "got ip: %s", ip4addr_ntoa(&event->ip_info.ip));
-        xEventGroupSetBits(s_connect_event_group, CONNECTED_BITS);
+        xEventGroupSetBits(s_connect_event_group, WIFI_CONNECTED_BITS);
     }
 }
 
@@ -161,7 +172,225 @@ static void wifi_connect_blocking(void)
     ESP_ERROR_CHECK(esp_wifi_start());
 
     // wait for connection
-    xEventGroupWaitBits(s_connect_event_group, CONNECTED_BITS, true, true, portMAX_DELAY);
+    xEventGroupWaitBits(s_connect_event_group, WIFI_CONNECTED_BITS, true, true, portMAX_DELAY);
+}
+
+
+void i2c_master_init()
+{
+	i2c_config_t i2c_config = {
+		.mode = I2C_MODE_MASTER,
+		.sda_io_num = SDA_PIN,
+		.scl_io_num = SCL_PIN,
+		.sda_pullup_en = GPIO_PULLUP_ENABLE,
+		.scl_pullup_en = GPIO_PULLUP_ENABLE,
+		.master.clk_speed = 1000000
+	};
+	i2c_param_config(I2C_NUM_0, &i2c_config);
+	i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+}
+
+s8_t BME280_I2C_bus_write(u8_t dev_addr, u8_t reg_addr, u8_t *reg_data, u8_t cnt)
+{
+	s32_t iError = 0;
+
+	esp_err_t espRc;
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_WRITE, true);
+
+	i2c_master_write_byte(cmd, reg_addr, true);
+	i2c_master_write(cmd, reg_data, cnt, true);
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		iError = 0;
+	} else {
+		iError = 1;
+	}
+	i2c_cmd_link_delete(cmd);
+
+	return (s8_t)iError;
+}
+
+s8_t BME280_I2C_bus_read(u8_t dev_addr, u8_t reg_addr, u8_t *reg_data, u8_t cnt)
+{
+	s32_t iError = 0;
+	esp_err_t espRc;
+
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_WRITE, true);
+	i2c_master_write_byte(cmd, reg_addr, true);
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (dev_addr << 1) | I2C_MASTER_READ, true);
+
+	if (cnt > 1) {
+		i2c_master_read(cmd, reg_data, cnt-1, I2C_MASTER_ACK);
+	}
+	i2c_master_read_byte(cmd, reg_data+cnt-1, I2C_MASTER_NACK);
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		iError = 0;
+	} else {
+		iError = 1;
+	}
+
+	i2c_cmd_link_delete(cmd);
+
+	return (s8_t)iError;
+}
+
+void BME280_delay_msek(u32_t msek)
+{
+	vTaskDelay(msek / portTICK_PERIOD_MS);
+}
+
+/*
+void task_bme280_normal_mode(void *ignore)
+{
+	struct bme280_t bme280 = {
+		.bus_write = BME280_I2C_bus_write,
+		.bus_read = BME280_I2C_bus_read,
+		.dev_addr = BME280_I2C_ADDRESS2,
+		.delay_msec = BME280_delay_msek
+	};
+
+	s32_t com_rslt;
+	s32_t v_uncomp_pressure_s32;
+	s32_t v_uncomp_temperature_s32;
+	s32_t v_uncomp_humidity_s32;
+
+	com_rslt = bme280_init(&bme280);
+
+	com_rslt += bme280_set_oversamp_pressure(BME280_OVERSAMP_16X);
+	com_rslt += bme280_set_oversamp_temperature(BME280_OVERSAMP_2X);
+	com_rslt += bme280_set_oversamp_humidity(BME280_OVERSAMP_1X);
+
+	com_rslt += bme280_set_standby_durn(BME280_STANDBY_TIME_1_MS);
+	com_rslt += bme280_set_filter(BME280_FILTER_COEFF_16);
+
+	com_rslt += bme280_set_power_mode(BME280_NORMAL_MODE);
+	if (com_rslt == SUCCESS) {
+		while(true) {
+			vTaskDelay(40 / portTICK_PERIOD_MS);
+
+			com_rslt = bme280_read_uncomp_pressure_temperature_humidity(
+				&v_uncomp_pressure_s32, &v_uncomp_temperature_s32, &v_uncomp_humidity_s32);
+
+			if (com_rslt == SUCCESS) {
+				ESP_LOGI(TAG_BME280, "%.2f degC / %.3f hPa / %.3f %%",
+					bme280_compensate_temperature_double(v_uncomp_temperature_s32),
+					bme280_compensate_pressure_double(v_uncomp_pressure_s32)/100, // Pa -> hPa
+					bme280_compensate_humidity_double(v_uncomp_humidity_s32));
+			} else {
+				ESP_LOGE(TAG_BME280, "measure error. code: %d", com_rslt);
+			}
+		}
+	} else {
+		ESP_LOGE(TAG_BME280, "init or setting error. code: %d", com_rslt);
+	}
+
+	vTaskDelete(NULL);
+}
+*/
+
+void print_sensor_data(struct bme280_data *comp_data) {
+#ifdef BME280_FLOAT_ENABLE
+        printf("FLOAT %0.2f, %0.2f, %0.2f\r\n",comp_data->temperature, comp_data->pressure, comp_data->humidity);
+#else
+        printf("INT %ld, %ld, %ld\r\n",comp_data->temperature, comp_data->pressure, comp_data->humidity);
+#endif
+}
+
+void stream_sensor_data_normal_mode() {
+	struct bme280_dev dev;
+	int8_t rslt = BME280_OK;
+
+	dev.dev_id = BME280_I2C_ADDR_PRIM;
+	dev.intf = BME280_I2C_INTF;
+	dev.read = BME280_I2C_bus_read;
+	dev.write = BME280_I2C_bus_write;
+	dev.delay_ms = BME280_delay_msek;
+
+	rslt = bme280_init(&dev);
+
+	uint8_t settings_sel;
+	struct bme280_data comp_data;
+
+	/* Recommended mode of operation: Indoor navigation */
+	dev.settings.osr_h = BME280_OVERSAMPLING_1X;
+	dev.settings.osr_p = BME280_OVERSAMPLING_16X;
+	dev.settings.osr_t = BME280_OVERSAMPLING_2X;
+	dev.settings.filter = BME280_FILTER_COEFF_16;
+	dev.settings.standby_time = BME280_STANDBY_TIME_1000_MS;
+
+	settings_sel = BME280_OSR_PRESS_SEL;
+	settings_sel |= BME280_OSR_TEMP_SEL;
+	settings_sel |= BME280_OSR_HUM_SEL;
+	settings_sel |= BME280_STANDBY_SEL;
+	settings_sel |= BME280_FILTER_SEL;
+	rslt = bme280_set_sensor_settings(settings_sel, &dev);
+	rslt = bme280_set_sensor_mode(BME280_NORMAL_MODE, &dev);
+
+	printf("Temperature, Pressure, Humidity\r\n");
+	while (1) {
+		/* Delay while the sensor completes a measurement */
+		dev.delay_ms(10);
+		rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, &dev);
+		print_sensor_data(&comp_data);
+	}
+
+	vTaskDelete(NULL);
+}
+
+void stream_sensor_data_forced_mode()
+{
+	struct bme280_dev dev;
+	int8_t rslt = BME280_OK;
+
+	dev.dev_id = BME280_I2C_ADDR_PRIM;
+	dev.intf = BME280_I2C_INTF;
+	dev.read = BME280_I2C_bus_read;
+	dev.write = BME280_I2C_bus_write;
+	dev.delay_ms = BME280_delay_msek;
+
+	rslt = bme280_init(&dev);
+    uint8_t settings_sel;
+	uint32_t req_delay;
+    struct bme280_data comp_data;
+
+    /* Recommended mode of operation: Indoor navigation */
+    dev.settings.osr_h = BME280_OVERSAMPLING_1X;
+    dev.settings.osr_p = BME280_OVERSAMPLING_16X;
+    dev.settings.osr_t = BME280_OVERSAMPLING_2X;
+    dev.settings.filter = BME280_FILTER_COEFF_16;
+
+    settings_sel = BME280_OSR_PRESS_SEL | BME280_OSR_TEMP_SEL | BME280_OSR_HUM_SEL | BME280_FILTER_SEL;
+
+    rslt = bme280_set_sensor_settings(settings_sel, &dev);
+	
+	/*Calculate the minimum delay required between consecutive measurement based upon the sensor enabled
+     *  and the oversampling configuration. */
+    req_delay = bme280_cal_meas_delay(&dev.settings);
+
+    printf("Temperature, Pressure, Humidity\r\n");
+    /* Continuously stream sensor data */
+    while (1) {
+        rslt = bme280_set_sensor_mode(BME280_FORCED_MODE, &dev);
+        /* Wait for the measurement to complete and print data @25Hz */
+        dev.delay_ms(req_delay);
+        rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, &dev);
+        print_sensor_data(&comp_data);
+    }
+
+	vTaskDelete(NULL);
 }
 
 
@@ -175,7 +404,12 @@ void app_main()
     }
     ESP_ERROR_CHECK( ret );
 
-    wifi_connect_blocking();
-    mqtt_app_start();
+    // wifi_connect_blocking();
+    // mqtt_app_start();
 
+	i2c_master_init();
+
+
+	TaskHandle_t xHandle = NULL;
+	xTaskCreate(stream_sensor_data_forced_mode, "stream_sensor_data", 2048, NULL, 6, &xHandle);
 }
